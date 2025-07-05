00cac0a09703e371633de86d6f0495d4b92b0027
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-07-05 22:12:32 +0530

    new setup with my button component

---
+++ src/components/MyButton.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Button, type ButtonProps } from "react-aria-components"
+
+export default function MyButton({
+  label,
+  ...props
+}: ButtonProps & { label: string }) {
+  return (
+    <Button
+      {...props}
+      className="cursor-pointer rounded-md bg-zinc-100 px-4 py-2 text-zinc-800 ring-zinc-100 ring-offset-2 ring-offset-zinc-800 outline-none hover:bg-zinc-100/90 focus-visible:ring-2"
+    >
+      {label}
+    </Button>
+  )
+}
