38a5198735983a5060c88e52d67adb7692997bee
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-07-06 16:06:28 +0530

    change button bg to green

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -9,7 +9,7 @@ export default function MyButton({
   return (
     <Button
       {...props}
-      className="cursor-pointer rounded-md bg-red-300 px-6 py-2 text-zinc-800 ring-zinc-100 ring-offset-2 ring-offset-zinc-800 outline-none hover:bg-zinc-100/90 focus-visible:ring-2"
+      className="cursor-pointer rounded-md bg-green-300 px-6 py-2 text-zinc-800 ring-zinc-100 ring-offset-2 ring-offset-zinc-800 outline-none hover:bg-zinc-100/90 focus-visible:ring-2"
     >
       {label}
     </Button>
